1e97debdb5fca019f782b8f64710ce870786a3b5
Nairobi <wyudha104@gmail.com>
2022-12-11 16:18:20 +0700

    menambah operator logika

--- src/operatorLogika.java
+++ src/operatorLogika.java
@@ -1,2 +1,29 @@
 public class operatorLogika {
+
+    public static void main (String[] args){
+        boolean a,b,c;
+//        OR / ||
+        System.out.println("======OR======");
+    a = true;
+    b = false;
+    c = (a || b);
+
+    System.out.println(c);
+    //      AND / &&
+        System.out.println("======OR======");
+    a = true;
+    b = false;
+    c = (a && b);
+
+    System.out.println(c);
+     //        XOR /  ^
+        System.out.println("======OR======");
+    a = true;
+    b = false;
+    c = (a ^ b);
+
+    System.out.println(c);
+
+
+    }
 }
