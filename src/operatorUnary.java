c6b32ac323a11d92b9d7bcabddb5120b8cd34259
Nairobi <wyudha104@gmail.com>
2022-12-11 16:18:46 +0700

    menambah operator Unary

--- src/operatorUnary.java
+++ src/operatorUnary.java
@@ -1,2 +1,23 @@
 public class operatorUnary {
+    public static void main (String[] args){
+//         Unary + dan -
+        int angka = 1;
+        System.out.printf("%d Unary + %d \n", angka, +angka);
+        System.out.printf("%d Unary - %d \n", angka, -angka);
+
+//        Unary decrement dan Increment
+        int angka2 = 10 ;
+        angka2++;
+        System.out.println("nilai Angka '++' = " + angka2);
+        int angka3 = 10 ;
+        angka3--;
+        System.out.println("nilai Angka '--' = " + angka3);
+//        Nilai Prefix
+        int a = 10;
+//        Nilai Postfix
+        System.out.printf("Nilai dengan 'Prefix' menjadi = %d \n ", ++a);
+        int b = 10;
+        System.out.printf("Nilai dengan 'Prefix' menjadi = %d \n ", b++);
+
+    }
 }
