ab3b57219c090a6475c5aa3b8d5fe2b3d7901f72
Nairobi <wyudha104@gmail.com>
2022-12-11 16:17:30 +0700

    menambah opertaor komperasi

--- src/operatorKomperasi.java
+++ src/operatorKomperasi.java
@@ -1,2 +1,25 @@
 public class operatorKomperasi {
+    public static  void main (String[] args) {
+    int a,b;
+    boolean hasilKomperasi;
+    a = 2;
+    b= 2;
+    hasilKomperasi = ( a == b);
+    System.out.printf("%d == %d --> %s \n",a,b, hasilKomperasi);
+//    Pertidaksamaan
+    a = 2;
+    b= 2;
+    hasilKomperasi = ( a != b);
+    System.out.printf("%d != %d --> %s \n",a,b, hasilKomperasi);
+//    kurang dari
+    a = 2;
+    b= 2;
+    hasilKomperasi = ( a < b);
+    System.out.printf("%d != %d --> %s \n",a,b, hasilKomperasi);
+//    lebih dari
+    a = 3;
+    b= 2;
+    hasilKomperasi = ( a >= b);
+    System.out.printf("%d >= %d --> %s \n",a,b, hasilKomperasi);
+    }
 }
