d77d6a27439bf3922653fb4a08d13cdffcf6e9bf
Nairobi <wyudha104@gmail.com>
2022-12-12 12:07:07 +0700

    modif

--- src/ifelseif.java
+++ src/ifelseif.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ifelseif {
+public class ifelseif {
+    public static void main (String[] args){
+
+
+        int kode = 20;
+
+        if (kode==20){
+            System.out.println("Kodisi if");
+        } else if (kode==10) {
+            System.out.println("Kodisi elseif");
+        } else {
+            System.out.println("Kodisi else");
+        }
+    }
 }
