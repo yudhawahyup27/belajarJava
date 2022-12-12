87c1843ab5cfcd9e8a9a9d3e55b0ad067fe85ce4
yudhawahyup27 <wyudha104@gmail.com>
2022-12-12 15:05:27 +0700

    Seselai method/fungsi

---
+++ src/fungsimethod.java
@@ -0,0 +1,11 @@
+public class fungsimethod {
+
+    public static void main (String[] args){
+    System.out.println("Hasilnya adalah"+ hitung(10,2));
+    }
+    public static float hitung( int panjang, int lebar){
+     int  hasil = panjang * lebar;
+    return hasil;
+    }
+
+}
