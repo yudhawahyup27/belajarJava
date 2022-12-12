e5f7a8ec61cc8d6357a436cf4317d97cb0cb4dad
Nairobi <wyudha104@gmail.com>
2022-12-12 11:45:15 +0700

    Belajar Kodisi If Else Selesai

---
+++ src/ifelse.java
@@ -0,0 +1,14 @@
+public  class ifelse {
+
+    public static void main(String[] args){
+        int a;
+        a = 10;
+
+        if (a == 10){
+            System.out.println("Kodisi If benar "+ a);
+        } else {
+            System.out.println("Kondisi Else");
+        }
+
+    }
+}
